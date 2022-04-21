75311c54b3cd366361b1bcf5c7d31f10ff08ac6d
Phillip <philliplee515@gmail.com>
2022-04-21 12:56:01 +0900

    models.py

--- network/models.py
+++ network/models.py
@@ -1,10 +1,41 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+MAX_LENGTH = 256
+
 
 class User(AbstractUser):
     pass
 
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text
+    text = models.CharField(max_length=MAX_LENGTH, default=None)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.text} on {self.post_date}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "text": self.text,
+            "timestamp": self.timestamp,
+        }
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} is following {self.following}"
